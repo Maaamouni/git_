187a6c5f85a6b75492756fb13b813a375e6f4092
outhmane <outhmane@kali.outhmane>
2026-02-01 01:33:56 +0100

    add dockerfile

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express")
-const app = express.
+const app = express()
 
 app.get("/api/v1", (req,res) => {
-	console.log("hey");
+	res.json([
+		{
+			"id":1,
+			"title":"Harry Potter"
+		}
+	])
 });
 
-app.listen(3000)
+app.listen(3000, () => {
+	console.log("Service is up")
+})
 //modifying the index.js for branche1
